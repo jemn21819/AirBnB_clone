75a49079b30fc43b557cd9cf9fb1ac2c0322e866
Lohkrii <joshualopez0429@gmail.com>
2021-02-12 18:15:50 +0000

    Task 7: Adding console functions (create, destroy, show, all, update)

--- console.py
+++ console.py
@@ -3,6 +3,17 @@
 
 """
 import cmd
+import json
+import shlex
+from sys import argv
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,10 +21,12 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
     intro = "Welcome to our AirBnb clone console!"
+    classes = {"BaseModel", "User", "Place", "City", "Amenity",
+            "State", "Review"}
 
-    """
-        Help documentation section.
-    """
+#
+#        Help documentation section.
+#
 
     def do_help(self, args):
         """
@@ -25,13 +38,62 @@ class HBNBCommand(cmd.Cmd):
         'help <command>' or '? <command>' gives help on <command>
 
         """
-# The only reason to define this method is for/
-# the help method to appear in the docstring
+        # The only reason to define this method is for/
+        # the help method to appear in the docstring
         cmd.Cmd.do_help(self, args)
 
-    """
-        Console interface and behavior.
-    """
+#
+#        Console interface and behavior.
+#
+
+    def do_create(self, cls):
+        """ Creates a new object based on BaseModel """
+        if len(cls) == 0 or cls == "":
+            return print("** class name missing **")
+        elif cls not in classes:
+            return print("** class doesn't exist")
+        else:
+            new_obj = eval(cls)()
+            new_obj.save()
+            print(new_obj.id)
+
+    def do_show(self, argv):
+        """ Prints the string representation of an instance
+        given the class name and id
+        """
+        args = shlex.split(argv)
+        if len(args) == 0 or args[0] == "":
+            return print("** class name missing **")
+        elif args[1] not in classes:
+            return print("** class doesn't exist **")
+        elif len(args) == 1 or args[1] == "":
+            return print("** instance id missing **")
+        else:
+            the_dict = storage.all()
+            key = args[0] + "." + args[1]
+            if key in the_dict:
+                print(the_dict[key])
+            else:
+                return print("** no instance found **")
+
+    def do_destroy(self, argv):
+        """ Destroys an instances given the class name & id """
+        args = shlex.split(argv)
+        if len(args) == 0 or args[0] = "":
+            return print("** class name missing **")
+        elif args[0] not in classes:
+            return print("** class doesn't exist **")
+        elif len(args) = 1 or args[1] = "":
+            return print("** instance id missing **")
+        else:
+            the_dict = storage.all()
+            key = args[0] + "." + args[1]
+            if key in the_dict:
+                del the_dict[key]
+                storage.save()
+            else:
+                return print("** instance not found **)
+
 
     def emptyline(self):
         """ Does nothing on (empty line + 'Enter') """
@@ -41,9 +103,9 @@ class HBNBCommand(cmd.Cmd):
         """ Prints new line after exit """
         print
 
-    """
-        EOF and quit Functions.
-    """
+#
+#        EOF and quit Functions.
+#
 
     def do_quit(self, line):
         """
