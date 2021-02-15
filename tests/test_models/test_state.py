b567585fcaacdd7b9939627473bbc19565d65cd9
James <jmartinez21834@stu.nuc.edu>
2021-02-15 16:24:33 +0000

    adding test for state class

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""
+Test for the User class
+"""
+
+
+from datetime import datetime
+import inspect
+from models import state
+from models.base_model import BaseModel
+import pep8
+import unittest
+State = state.State
+
+c = "created_at"
+u = "updated_at"
+
+
+class TestStateDocStyle(unittest.TestCase):
+    """test for documentation and pep8 style"""
+    def setUp(cls):
+        """set up for methods"""
+        cls.state_func = inspect.getmembers(State, inspect.isfunction)
+
+    def test_pep8(self):
+        """test pep8 in State class"""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors and warnings.")
+
+    def test_pep8_test(self):
+        """test pep8 for test to state class"""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors and warnings.")
+
+    def test_module_doc(self):
+        """test for the doc in module"""
+        self.assertIsNot(state.__doc__, None,
+                         "State.py without a docstring")
+        self.assertTrue(len(state.__doc__) >= 1,
+                        "State.py without a docstring")
+
+    def test_class_doc(self):
+        self.assertIsNot(State.__doc__, None,
+                         "State class without docstring")
+        self.assertTrue(len(State.__doc__) >= 1,
+                        "State class without docstring")
+
+    def test_func_doc(self):
+        """test for doc in methods"""
+        for func in self.state_func:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} docstring needed in method".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} docstring needed in method".format(func[0]))
+
+
+class TestUserClass(unittest.TestCase):
+    """test for class"""
+    def test_is_subclass(self):
+        """test for a subclass of BaseModel"""
+        state = State()
+        self.assertIsInstance(state, BaseModel)
+        self.assertTrue(hasattr(state, "id"))
+        self.assertTrue(hasattr(state, "created_at"))
+        self.assertTrue(hasattr(state, "updated_at"))
+
+    def test_name(self):
+        """test for the state_id attr"""
+        state = State()
+        self.assertTrue(hasattr(state, "name"))
+        self.assertEqual(state.name, "")
+
+    def test_to_dict_values(self):
+        """test the values in dict"""
+        time = "%Y-%m-%dT%H:%M:%S.%f"
+        state = State()
+        new_dict = state.to_dict()
+        self.assertEqual(new_dict["__class__"], "State")
+        self.assertEqual(type(new_dict[c]), str)
+        self.assertEqual(type(new_dict[u]), str)
+        self.assertEqual(new_dict[c], state.created_at.strftime(time))
+        self.assertEqual(new_dict[u], state.updated_at.strftime(time))
+
+    def test_str(self):
+        """test for output str method"""
+        state = State()
+        string = "[State] ({}) {}".format(state.id, state.__dict__)
+        self.assertEqual(string, str(state))
