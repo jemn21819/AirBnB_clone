f9db3cc88a6f415e6b737277074ffaba13a7ae4c
James <jmartinez21834@stu.nuc.edu>
2021-02-16 12:08:51 +0000

    fixin pep8

--- models/base_model.py
+++ models/base_model.py
@@ -21,10 +21,10 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-                if hasattr(self, c) and type(self.created_at) is str:
-                    self.created_at = datetime.strptime(kwargs[c], t)
-                if hasattr(self, u) and type(self.updated_at) is str:
-                    self.updated_at = datetime.strptime(kwargs[u], t)
+            if hasattr(self, c) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs[c], t)
+            if hasattr(self, u) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs[u], t)
 
         else:
             self.id = str(uuid.uuid4())
