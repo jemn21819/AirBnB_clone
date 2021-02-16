6790d3b2280332d1d13efd83b7849d9a6094c968
Lohkrii <joshualopez0429@gmail.com>
2021-02-16 20:55:52 +0000

    console.py

--- console.py
+++ console.py
@@ -130,7 +130,6 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """ --- quit help documentation ---
         The quit function closes the console gracefully """
-        print()
         return True
 
     def do_EOF(self, line):
