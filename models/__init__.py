906143705208b0b8d8e579abd07dfdb047fa9b3b
James <jmartinez21834@stu.nuc.edu>
2021-02-17 04:19:24 +0000

    modifiying consol

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,24 @@ initialize the models package
 """
 
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
+
+class_dict = {
+    'BaseModel': BaseModel,
+    'State': State,
+    'City': City,
+    'Amenity': Amenity,
+    'Place': Place,
+    'Review': Review,
+    'User': User
+}
+
 
 storage = FileStorage()
 storage.reload()
