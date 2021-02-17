46560c54a1d1f6eb299e682dba9e19119feb59d3
James <jmartinez21834@stu.nuc.edu>
2021-02-17 04:04:24 +0000

    modifiying consol

--- models/__init__.py
+++ models/__init__.py
@@ -5,16 +5,5 @@ initialize the models package
 
 from models.engine.file_storage import FileStorage
 
-
-classes = {
-    'BaseModel': BaseModel,
-    'User': User,
-    'State': State,
-    'City': City,
-    'Amenity': Amenity,
-    'Place': Place,
-    'Review': Review
-}
-
 storage = FileStorage()
 storage.reload()
