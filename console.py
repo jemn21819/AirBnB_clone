ff24d4f7e4d0bf6320d3346d32b1b38ff1b377ac
James <jmartinez21834@stu.nuc.edu>
2021-02-17 04:17:16 +0000

    modifiying consol

--- console.py
+++ console.py
@@ -5,6 +5,23 @@ import cmd
 from datetime import datetime
 import models
 import re
+from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
+
+class_dict = {
+    'BaseModel': BaseModel,
+    'State': State,
+    'City': City,
+    'Amenity': Amenity,
+    'Place': Place,
+    'Review': Review,
+    'User': User
+}
 
 
 class HBNBCommand(cmd.Cmd):
